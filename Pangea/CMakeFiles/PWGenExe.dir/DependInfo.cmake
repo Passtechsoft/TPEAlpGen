18ed3c62d1ef8ae3aab0f767b569bcb79c242b50
{Alexis Gros;creeperdeking;tewos} <alexis.gros99@gmail.com>
2015-10-22 12:40:12 +0200

    création du cmake basique, compile.

---
+++ Pangea/CMakeFiles/PWGenExe.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/creeps/Documents/TPEAlpGen/Pangea/source/Cell.cpp" "/home/creeps/Documents/TPEAlpGen/Pangea/CMakeFiles/PWGenExe.dir/source/Cell.cpp.o"
+  "/home/creeps/Documents/TPEAlpGen/Pangea/source/IO.cpp" "/home/creeps/Documents/TPEAlpGen/Pangea/CMakeFiles/PWGenExe.dir/source/IO.cpp.o"
+  "/home/creeps/Documents/TPEAlpGen/Pangea/source/World.cpp" "/home/creeps/Documents/TPEAlpGen/Pangea/CMakeFiles/PWGenExe.dir/source/World.cpp.o"
+  "/home/creeps/Documents/TPEAlpGen/Pangea/source/Zone.cpp" "/home/creeps/Documents/TPEAlpGen/Pangea/CMakeFiles/PWGenExe.dir/source/Zone.cpp.o"
+  "/home/creeps/Documents/TPEAlpGen/Pangea/source/main.cpp" "/home/creeps/Documents/TPEAlpGen/Pangea/CMakeFiles/PWGenExe.dir/source/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Pangea/CMakeFiles/PWGenExe.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PWGenExe.dir/source/IO.cpp.o"
+  "CMakeFiles/PWGenExe.dir/source/World.cpp.o"
+  "CMakeFiles/PWGenExe.dir/source/Zone.cpp.o"
+  "CMakeFiles/PWGenExe.dir/source/Cell.cpp.o"
+  "CMakeFiles/PWGenExe.dir/source/main.cpp.o"
+  "PWGenExe.pdb"
+  "PWGenExe"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PWGenExe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
