8623d86f84e06b56d75992aaf552d0d2fb1a4928
Porteries Tristan <republicthunderbolt9@gmail.com>
2015-10-22 14:33:25 +0200

    Debut d'implementation de la classe Cell.

--- Pangea/source/Cell.h
+++ Pangea/source/Cell.h
@@ -1,1 +1,26 @@
- 
+class Cell
+{
+public:
+	enum NatureType {
+		NAT_BORD,
+		NAT_SOCLE_OCEAN,
+		NAT_COUCHE_INTERSTI,
+		NAT_SOCLE_CONTI,
+		NAT_SOCLE_CONTI_ERO,
+	};
+
+	Cell(unsigned int color,
+		 NatureType nature,
+		 float courbure,
+		 float vieil,
+		 float altitude);
+	~Cell();
+
+private:
+	unsigned int m_color;
+	NatureType m_nature;
+	float m_courbure;
+	unsigned int m_memoire;
+	float m_vieil;
+	float m_altitude;
+};
--- Pangea/source/main.cpp
+++ Pangea/source/main.cpp
@@ -1,1 +1,6 @@
- 
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
